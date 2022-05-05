0481b052e9af41910f49309922bbe286a04305fa
Di <daodawich@gmail.com>
2022-05-05 18:56:10 +0600

    rework header 18:56

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,89 +22,94 @@
 
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm" style="border-bottom: 2px solid hotpink;">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-{{--                    {{ config('app.name', 'Laravel') }}--}}
-{{--                    min-width: 20px; min-height: 20px; border: 2px solid black; border-radius: 20px;--}}
-                    <div style="">
-                        <h1 style="margin:5px; color: hotpink">СКАЗКА</h1>
-                    </div>
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+    <div class="background-back" id="app">
+        <header>
+            <nav class=" container navbar navbar-expand-md header-top" style=" background: transparent;">
+                <div class="container glass">
+                    <a class="navbar-brand" href="{{ url('/') }}">
+                        {{--                    {{ config('app.name', 'Laravel') }}--}}
+                        {{--                    min-width: 20px; min-height: 20px; border: 2px solid black; border-radius: 20px;--}}
+                        <div style="">
+                            <h1 style="margin:5px; color: white">СКАЗКА</h1>
+                        </div>
+                    </a>
+                    <button class="navbar-toggler main-colors" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}" style="padding:0px 5px 5px 0px;">
+                    <span class="navbar-toggler-icon">
+                         <img src="public/images/svg/list-menu-hand-drawn-symbol-of-three-thin-rectangles-outlines-svgrepo-com.svg" alt="My SVG Icon" style="width: 35px; padding:0;">
+                    </span>
+                    </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <!-- Left Side Of Navbar -->
+                        <ul class="navbar-nav me-auto">
 
-                    </ul>
+                        </ul>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
-                        <!-- Authentication Links -->
-{{--                        <li class="nav-item">--}}
-{{--                            <a class="nav-link" href="/">Каталог</a>--}}
-{{--                        </li>--}}
+                        <!-- Right Side Of Navbar -->
+                        <ul class="navbar-nav ms-auto">
+                            <!-- Authentication Links -->
+                            {{--                        <li class="nav-item">--}}
+                            {{--                            <a class="nav-link" href="/">Каталог</a>--}}
+                            {{--                        </li>--}}
 
-                        <li class="nav-item">
-                            <a class="nav-link" href="/" style="color: hotpink;">
-                                <img src="public/images/svg/list-paper-hand-drawn-symbol-svgrepo-com.svg" alt="My SVG Icon" style="width: 25px; filter: invert(57%) sepia(54%) saturate(1447%) hue-rotate(296deg) brightness(102%) contrast(101%);">
-                                О нас
-                            </a>
-                        </li>
-
-
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-{{--                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>--}}
-
-                                    <a class="nav-link" href="{{ route('login') }}" style="color: hotpink;">
-
-                                        <img src="public/images/svg/next-user-hand-drawn-interface-symbol-svgrepo-com.svg" alt="My SVG Icon" style="width: 25px; filter: invert(57%) sepia(54%) saturate(1447%) hue-rotate(296deg) brightness(102%) contrast(101%);">
-                                        Войти
-                                    </a>
-                                </li>
-                            @endif
-
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}" style="color: hotpink;">
-                                        <img src="public/images/svg/users-couple-hand-drawn-outlines-svgrepo-com.svg" alt="My SVG Icon" style="width: 25px; filter: invert(57%) sepia(54%) saturate(1447%) hue-rotate(296deg) brightness(102%) contrast(101%);">
-                                        Регистрация
-                                    </a>
-                                </li>
-                            @endif
-                        @else
-
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
+                            <li class="nav-item">
+                                <a class="nav-link" href="/" style="color:white;">
+                                    <img class="main-colors" src="public/images/svg/list-paper-hand-drawn-symbol-svgrepo-com.svg" alt="My SVG Icon" style="width: 25px;">
+                                    О нас
                                 </a>
+                            </li>
 
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
+
+                            @guest
+                                @if (Route::has('login'))
+                                    <li class="nav-item">
+                                        {{--                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>--}}
+
+                                        <a class="nav-link" href="{{ route('login') }}" style="color: white;">
+
+                                            <img class="main-colors" src="public/images/svg/next-user-hand-drawn-interface-symbol-svgrepo-com.svg" alt="My SVG Icon" style="width: 25px;">
+                                            Войти
+                                        </a>
+                                    </li>
+                                @endif
+
+                                @if (Route::has('register'))
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="{{ route('register') }}" style="color: white;">
+                                            <img class="main-colors" src="public/images/svg/users-couple-hand-drawn-outlines-svgrepo-com.svg" alt="My SVG Icon" style="width: 25px;">
+                                            Регистрация
+                                        </a>
+                                    </li>
+                                @endif
+                            @else
+
+                                <li class="nav-item dropdown">
+                                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                        {{ Auth::user()->name }}
                                     </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
+                                    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="{{ route('logout') }}"
+                                           onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                            {{ __('Logout') }}
+                                        </a>
 
-                    </ul>
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                            @csrf
+                                        </form>
+                                    </div>
+                                </li>
+                            @endguest
+
+                        </ul>
+                    </div>
                 </div>
-            </div>
-        </nav>
+            </nav>
+        </header>
 
-        <main class="py-4 background-back ">
+        <div style="width: 100%; height: 70px"></div>
+        <main class="py-4" style="height: 100vh;">
             @yield('content')
         </main>
     </div>
